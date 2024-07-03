a2bc5d678bee719757be52aa619a0d0311b0845d
Matthew Setter <matthew@matthewsetter.com>
2024-07-03 17:32:48 +1000

    Finish implementing login functionality
    While a bit rough, this is the first implementation of the login
    functionality, where the user can both load the login form and submit
    it, whereupon, if their username is found and from it their phone
    number, a verification code will be sent via SMS. As I said, it's a
    little rough, but it works. A refined implementation will be added
    shortly.

--- src/main/java/org/settermjd/LoginVerification/HelloWorld.java
+++ src/main/java/org/settermjd/LoginVerification/HelloWorld.java
@@ -1,16 +1,98 @@
 package org.settermjd.LoginVerification;
 
+import com.twilio.Twilio;
+import com.twilio.rest.verify.v2.service.Verification;
+
 import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.glassfish.grizzly.http.util.HttpStatus;
 import spark.ModelAndView;
+import spark.Redirect;
 import spark.template.velocity.VelocityTemplateEngine;
 
 import static spark.Spark.*;
 
 public class HelloWorld {
+    private static final Logger logger = LogManager.getLogger(HelloWorld.class);
+
+    private static String getPhoneNumberForUsername(String username) {
+        String findMatchingUserQuery = "SELECT phone_number FROM user WHERE username = ?";
+        String dsn = "jdbc:sqlite:src/main/resources/database/database.sqlite";
+        try (Connection connection = DriverManager.getConnection(dsn);
+             PreparedStatement statement = connection.prepareStatement(findMatchingUserQuery);) {
+            statement.setQueryTimeout(30);
+            statement.setString(1, username);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                return rs.getString("phone_number");
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+        }
+
+        return "";
+    }
+
     public static void main(String[] args) {
         port(8080);
+
+        Dotenv dotenv = null;
+        dotenv = Dotenv.configure().load();
+
+        String accountSID = dotenv.get("TWILIO_ACCOUNT_SID");
+        String authToken = dotenv.get("TWILIO_AUTH_TOKEN");
+        String verifySID = dotenv.get("VERIFY_SERVICE_SID");
+
+        staticFiles.location("/public");
+        staticFiles.expireTime(600);
+
+        path("/login", () -> {
+            before("/*", (q, a) -> logger.info("Received login request"));
+            get("", (request, response) -> {
+                Map<String, Object> model = new HashMap<>();
+                String error = request.session().attribute("error");
+                if (error != null && ! error.isEmpty()) {
+                    model.put("error", error);
+                }
+                return new ModelAndView(model, "templates/login.vm");
+            }, new VelocityTemplateEngine());
+            post("", (request, response) -> {
+                String username = request.queryParams("username");
+                if (username == null || username.isEmpty()) {
+                    String error = "Username not available";
+                    request.session().attribute("error", error);
+                    logger.error(error);
+                    response.redirect("/login", Redirect.Status.SEE_OTHER.intValue());
+                    return null;
+                }
+
+                String phoneNumber = getPhoneNumberForUsername(username);
+                if (Objects.equals(phoneNumber, "")) {
+                    request.session().attribute("error", "Could not find user's phone number");
+                    response.redirect("/login", Redirect.Status.SEE_OTHER.intValue());
+                    return null;
+                }
+
+                Twilio.init(accountSID, authToken);
+                Verification
+                        .creator(verifySID, phoneNumber, "sms")
+                        .create();
+                response.redirect("/verifyme", Redirect.Status.SEE_OTHER.intValue());
+                return null;
+            });
+        });
+
+        get("/verifyme", ((request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "templates/verifyme.vm");
+        }), new VelocityTemplateEngine());
 
         get("/profile", ((request, response) -> {
             Map<String, Object> model = new HashMap<>();
